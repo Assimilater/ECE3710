7d3153757aa675272fc1ee4ecc822751bf491a29
John Call <johnkc1992@gmail.com>
2015-10-28 08:43:12 -0600

    just some function prototypes

--- Lab 4/main.c
+++ Lab 4/main.c
@@ -5,8 +5,8 @@ volatile int PD_DIR_R __attribute__((at(0x40007400)));
 volatile int PD_AF_R __attribute__((at(0x40007420)));
 volatile int PD_DEN_R __attribute__((at(0x4000751C)));
 volatile int RCGC2_R __attribute__((at(0x400FE108)));
-#define RCGC2_PD      0x00000008  //value for RCGC2 to enable clock for port D
 
+#define RCGC2_PD		0x00000008  //value for RCGC2 to enable clock for port D
 
 #define GPIO_DIR		0x0400
 #define GPIO_AFSEL		0x0420
@@ -26,6 +26,7 @@ unsigned char* PA = (unsigned char*)0x40004000;
 unsigned char* UART0 = (unsigned char*)0x4000C000;
 unsigned char UART0_D __attribute__((at(0x4000C000)));
 volatile unsigned int UART0_STAT __attribute__((at(0x4000C018)));
+
 #define UART_FLAG		0x018
 #define UART_IBRD		0x024
 #define UART_FBRD		0x028
@@ -34,6 +35,23 @@ volatile unsigned int UART0_STAT __attribute__((at(0x4000C018)));
 #define UART_CC			0xFC8
 
 #define GPIO_UNLOCK		0x4C4F434B
+
+void GenTable() {
+
+}
+
+char keyToAscii(char in) {
+
+	return 0;
+}
+
+void keyHandler() {
+
+}
+
+void TXAvailableHandler() {
+
+}
 
 void Init() {
 	// enable clock: uart then ports
@@ -69,19 +87,13 @@ void Init() {
 	// 6. enable tx rx and uart
 	//UART0[UART_CTL+1] = 0x3;
 	UART0_CTL_R = 0x301;
+
+	// Configure handlers
 }
 
 int main(void) {
-	unsigned char z;
-
 	Init();
-
-
-
-	while (z != 50) {
-		for (z = 0; z<16; z++)
-			PD_DATA_R = z;
-	}
+	GenTable();
 
 	return 1;
 }
