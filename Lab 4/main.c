868f4555e238309c658c9c406624357bcd569167
Landon Chaz Wilcox <Landon.Wilcox@outlook.com>
2015-10-28 21:36:06 -0600

    changed pins we're using in port A to 2 and 3, configured PB1 for UART Tx

--- Lab 4/main.c
+++ Lab 4/main.c
@@ -23,7 +23,7 @@ unsigned char ascii(unsigned char in) { return in > 0x38 ? 0 : ps2_to_ascii[in-0
 //---------------------------------------------------------------------------------------+
 // Pointers for managing the ascii buffer (that is to say, the key log)                  |
 //---------------------------------------------------------------------------------------+
-// Insert variables used in UART0, and GPIOA
+// Insert variables used in UART1, and GPIOA
 
 //---------------------------------------------------------------------------------------+
 // Interrupt handler used to add characters to the key log buffer                        |
@@ -36,12 +36,12 @@ void GPIOA_Handler() {
 //---------------------------------------------------------------------------------------+
 // Interrupt handler used fill the TX buffer when there's room and data left to send     |
 //---------------------------------------------------------------------------------------+
-void UART0_Handler() {
-	UART0_ICR_R = 0x20; // Clears interrupt
+void UART1_Handler() {
+	UART1_ICR_R = 0x20; // Clears interrupt
 	
 	// Check bit 5 for txfull flag and ascii buffer for remaining data to send
-	while (!(UART0_FR_R & 0x20) && 1) { // TODO: Check if ascii stream is finished
-		UART0_DR_R = 0; // enter byte to write
+	while (!(UART1_FR_R & 0x20) && 1) { // TODO: Check if ascii stream is finished
+		UART1_DR_R = 0; // enter byte to write
 	}
 }
 
@@ -63,34 +63,38 @@ void GPIOF_Handler() {
 		// Turn light red
 		// Disable GPIOA_Handler
 		GPIO_PORTA_IM_R = 0;
-		UART0_Handler();
+		UART1_Handler();
 	}
 }
 
 void Init() {
 	
-	//PA0 is keylogger data
-	//PA1 is UART0 Tx
 	//PA2 is keyboard clock
+	//PA3 is keylogger data	
+	//PB1 is UART1 Tx
 	//PF0 is button
 	
 	// enable clocks
-	SYSCTL_RCGC1_R = 0x1; //uart0
+	SYSCTL_RCGC1_R = 0x2; //UART1
 	SYSCTL_RCGC2_R = 0x21; //portA and F
 	
 	GPIO_PORTB_LOCK_R = GPIO_UNLOCK; //unlock portF
 
 	// configure port A
-	GPIO_PORTA_CR_R = 0x7;
-	GPIO_PORTA_AFSEL_R = 0x2;
-	GPIO_PORTA_DIR_R = 0x1;
-	GPIO_PORTA_PUR_R = 0x1;
-	GPIO_PORTA_DEN_R = 0x7;
+	GPIO_PORTA_CR_R = 0xC;
+	GPIO_PORTA_DIR_R = 0x8;
+	GPIO_PORTA_PUR_R = 0x8;
+	GPIO_PORTA_DEN_R = 0xC;
 	GPIO_PORTA_IM_R = 0; //disable port A's interrupt handler while configuring
 	GPIO_PORTA_IS_R = 0; //Sets interrupt to detect edge
 	GPIO_PORTA_IBE_R = 0; //interrupt only detects one edge
 	GPIO_PORTA_IEV_R = 0; //detects negative edge (button is active-low)
 	GPIO_PORTA_IM_R = 0x4; //enables interrupts for PA2
+	
+	// configure port B
+	GPIO_PORTA_CR_R = 0x2;
+	GPIO_PORTA_AFSEL_R = 0x2;
+	GPIO_PORTA_DEN_R = 0x2;
 	
 	// configure port F
 	GPIO_PORTF_CR_R = 0x1;
@@ -109,21 +113,21 @@ void Init() {
 	
 	NVIC_PRI0_R = 0xE0; //Sets priority of Port A to 7
 
-	// disable uart0
-	UART0_CTL_R = 0x0;
+	// disable UART1
+	UART1_CTL_R = 0x0;
 
 	// BRD = 16e6/(16*9600)= 104.1667
-	UART0_IBRD_R = 104;	// integer portion: int(104.1667)=104
-	UART0_FBRD_R = 0xB;	// fractional portion: int(.1667*2^6+0.5)=11
+	UART1_IBRD_R = 104;	// integer portion: int(104.1667)=104
+	UART1_FBRD_R = 0xB;	// fractional portion: int(.1667*2^6+0.5)=11
 
 	// set serial parameters
-	UART0_LCRH_R = 0x70; //FIFO enabled, 8-bit word
+	UART1_LCRH_R = 0x70; //FIFO enabled, 8-bit word
 	
-	UART0_IFLS_R = 0x3; // bit in UARTRIS_R is set when Tx FIFO <= 1/4 full
-	UART0_IM_R = 0x20; // enables interrupts for Tx
+	UART1_IFLS_R = 0x3; // bit in UARTRIS_R is set when Tx FIFO <= 1/4 full
+	UART1_IM_R = 0x20; // enables interrupts for Tx
 	
 	// enable tx and uart
-	UART0_CTL_R = 0x101;
+	UART1_CTL_R = 0x101;
 
 }
 
