c098972e942ba6354e051f16dde772a849ed4e5c
John Call <johnkc1992@gmail.com>
2015-10-28 21:06:47 -0600

    Add some documentation, remove unused compiler constants, implement a form of bool, add some logic to UART handler

--- Lab 4/main.c
+++ Lab 4/main.c
@@ -1,44 +1,18 @@
 #include "../Shared/Register_Defs_C.h"
 
-volatile int PD_DATA_R __attribute__((at(0x400073FC)));
-volatile int PD_DIR_R __attribute__((at(0x40007400)));
-volatile int PD_AF_R __attribute__((at(0x40007420)));
-volatile int PD_DEN_R __attribute__((at(0x4000751C)));
-volatile int RCGC2_R __attribute__((at(0x400FE108)));
+#define GPIO_UNLOCK 0x4C4F434B
 
-#define RCGC2_PD		0x00000008  //value for RCGC2 to enable clock for port D
+//---------------------------------------------------------------------------------------+
+// Boolean implementation (minus the nice data compaction afforded to c++)               |
+//---------------------------------------------------------------------------------------+
+typedef char bool;
+#define true 1
+#define false 0
 
-#define GPIO_DIR		0x0400
-#define GPIO_AFSEL		0x0420
-#define GPIO_PUR		0x0510
-#define GPIO_DEN		0x051C
-#define GPIO_LOCK		0x0520
-#define GPIO_PCTL		0x052C
-#define GPIO_CR			0x0524
-
-// system control base addr
-unsigned int* SYSCTL = (unsigned int*)0x400FE000;
-
-//UART0 shares pins with Port A (RX: PA0, TX: PA1)
-unsigned char* PA = (unsigned char*)0x40004000;
-
-// UART0 base
-unsigned char* UART0 = (unsigned char*)0x4000C000;
-unsigned char UART0_D __attribute__((at(0x4000C000)));
-volatile unsigned int UART0_STAT __attribute__((at(0x4000C018)));
-
-#define UART_FLAG		0x018
-#define UART_IBRD		0x024
-#define UART_FBRD		0x028
-#define UART_LCRH		0x02C
-#define UART_CTL		0x030
-#define UART_CC			0xFC8
-
-#define GPIO_UNLOCK		0x4C4F434B
-
-// This is a look up table for basic key strokes. 
-// This translates from ps/2 to ascii.
-// The table starts at 0x15, and ends at 0x4D (meaning last index is 0x38)
+//---------------------------------------------------------------------------------------+
+// Lookup table for converting ps/2 key strokes to ascii                                 |
+// The table starts at 0x15, and ends at 0x4D (meaning last index is 0x38)               |
+//---------------------------------------------------------------------------------------+
 unsigned char ps2_to_ascii[] = {
 	'q','1',0x00,0x00,0x00,'z','s','a','w','2',0x00,
 	0x00,'c','x','d','e','4','3',0x00,0x00,' ','v','f','t','r','5',0x00,0x00,
@@ -46,20 +20,35 @@ unsigned char ps2_to_ascii[] = {
 	0x00,',','k','i','o','0','9',0x00,0x00,'.',0x00,'l','p'};
 unsigned char ascii(unsigned char in) { return in > 0x38 ? 0 : ps2_to_ascii[in-0x25]; }
 
+
+//---------------------------------------------------------------------------------------+
+// Interrupt handler used to add characters to the key log buffer                        |
+//---------------------------------------------------------------------------------------+
 void GPIOA_Handler() {
 	// Keyboard Clock Handler
 	
 }
 
+//---------------------------------------------------------------------------------------+
+// Interrupt handler used fill the TX buffer when there's room and data left to send     |
+//---------------------------------------------------------------------------------------+
 void UART0_Handler() {
-	// UART Tx Handler
-	UART0_ICR_R = 0x20; //Tx interrupt clear
+	//Tx interrupt clear
+	UART0_ICR_R = 0x20;
 	
-	UART0_FR_R; //check bit 5 for full tx
+	// check bit 5 for full tx and fill until full
+	while (!(UART0_FR_R & 0x20) && 1) { // TODO: Check if ascii stream is finished
+		UART0_DR_R = 0; // enter byte to write
+	}
 }
 
+//---------------------------------------------------------------------------------------+
+// Interrupt handler used respond to a button press                                      |
+//---------------------------------------------------------------------------------------+
 void GPIOF_Handler() {
+	static bool enabled = false;
 	// Button handler
+	
 	
 }
 
