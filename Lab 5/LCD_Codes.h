5d57943bbfa28efb26c18fc675047be308e77dae
John Call <johnkc1992@gmail.com>
2015-11-05 14:47:21 -0700

    Device a system to easily manage all these command codes

---
+++ Lab 5/LCD_Codes.h
@@ -0,0 +1,10 @@
+#ifndef __TFTM032_CODES_H__
+#define __TFTM032_CODES_H__
+
+typedef const unsigned char code;
+//---------------------------------------------------------------------------------------+
+// A collection of code sizes to keep the extern definition the same                     |
+//---------------------------------------------------------------------------------------+
+#define SIZE_CODE_PWRA          6
+
+#endif
