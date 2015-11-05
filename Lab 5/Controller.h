b6490cebb48af71c56b1db957dee68ceaf0ec6dc
Landon Chaz Wilcox <Landon.Wilcox@outlook.com>
2015-11-05 12:23:51 -0700

    Added comments

--- Lab 5/Controller.h
+++ Lab 5/Controller.h
@@ -2,9 +2,9 @@
 #define __TM4C123GH6PM_H__
 
 // PD: 0x42000000 + 32*0x73FC = 0x420E7F80
-#define LCD_CS (*((volatile unsigned int *)0x420E7F88))
-#define DCX (*((volatile unsigned int *)0x420E7F8C))
-#define WRX (*((volatile unsigned int *)0x420E7F98))
-#define RDX (*((volatile unsigned int *)0x420E7F9C))
+#define LCD_CS (*((volatile unsigned int *)0x420E7F88)) // PD2 = 0x420E7F88
+#define DCX (*((volatile unsigned int *)0x420E7F8C)) // PD3 = 0x420E7F8C
+#define WRX (*((volatile unsigned int *)0x420E7F98)) // PD6 = 0x420E7F98
+#define RDX (*((volatile unsigned int *)0x420E7F9C)) // PD7 = 0x420E7F9C
 
 #endif
