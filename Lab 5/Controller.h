2b673b6719a7c5b52b6c959d6a0e1a0dd2b4e8d0
Landon Chaz Wilcox <Landon.Wilcox@outlook.com>
2015-11-05 12:21:24 -0700

    Added Bit Banding for port D

--- Lab 5/Controller.h
+++ Lab 5/Controller.h
@@ -1,7 +1,10 @@
 #ifndef __TM4C123GH6PM_H__
 #define __TM4C123GH6PM_H__
 
-
-
+// PD: 0x42000000 + 32*0x73FC = 0x420E7F80
+#define LCD_CS (*((volatile unsigned int *)0x420E7F88))
+#define DCX (*((volatile unsigned int *)0x420E7F8C))
+#define WRX (*((volatile unsigned int *)0x420E7F98))
+#define RDX (*((volatile unsigned int *)0x420E7F9C))
 
 #endif
--- Lab 5/main.c
+++ Lab 5/main.c
@@ -1,6 +1,8 @@
 #include "Controller.h"
 #include "LCD.h"
 
+
+
 int main() {
 	
 	return 0;
