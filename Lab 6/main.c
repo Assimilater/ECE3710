6f7463d4b794da99b9df758e5edd98c88cc1a126
Landon Chaz Wilcox <Landon.Wilcox@outlook.com>
2015-11-17 18:04:43 -0700

    Finished ADC0 init

--- Lab 6/main.c
+++ Lab 6/main.c
@@ -41,41 +41,38 @@ PLL_Validate
 void init() {
 	useXTAL();
 	
-	//SYSCTL_RCGC1_R |= 0x10000; //GPTM0
-	SYSCTL->RCGCTIMER = 0x7; //Enable timers 0, 1, 2
-	//SYSCTL_RCGC2_R |= 0x8; //port D
+	//SYSCTL_RCGC1_R |= 0x10000; // GPTM0
+	SYSCTL->RCGCTIMER = 0x7; // Enable timers 0, 1, 2
+	//SYSCTL_RCGC2_R |= 0x8; // port D
 	SYSCTL->RCGCGPIO = 0x1B;
 	//SYSCTL_RCGC0_R |= 0x10000; //ADC0
-	SYSCTL->RCGCADC = 0x1; //enable ADC0
+	SYSCTL->RCGCADC = 0x1; // enable ADC0
 	//SYSCTL->RCGCSSI = 0x1;
 	//GPIO.PortD->LOCK.word = GPIO_UNLOCK;
 		// enable clocks
 	
 	//config PD
 	//PD0 is AIN7
-	//GPIO_PORTD_AFSEL_R |= 0x1; //enable AF for PD0
-	GPIO.PortD->AFSEL.word = 0x1; //enable AF for PD0
-	//GPIO_PORTD_DEN_R = 0; //Set PD as analog input
-	GPIO.PortD->DEN.bit0 = 0;
-	/GPIO_PORTD_AMSEL_R |= 0x1; //disable isolation for PD0 for analog
-	GPIO.PortD->AM
-	//GPIO_PORTD_ADCCTL_R |= 0x1; //PD0 triggers ADC
+	GPIO.PortD->AFSEL.word = 0x1; // enable AF for PD0
+	GPIO.PortD->DEN.bit0 = 0; // Set PD as analog input
+	GPIO.PortD->AMSEL.word = 0x1; // disable isolation for PD0 for analog
+	GPIO.PortD->ADCCTL.word = 0x1; // PD0 triggers ADC
 	
 	//config ADC
-	//ADC0_ACTSS_R = 0; //Disable ADC0
-	//ADC0_EMUX_R |= 0x5; //Timer triggers interrupt for sequencer0
-	//ADC0_SSCTL0_R |= 0x6; //one sample sequence
-	//ADC0_IM_R |= 0x1; //enable interrupts for SS0
-	//ADC0_ACTSS_R |= 0x1; //enable SS0 in ADC0
+	ADC0->ACTSS = 0; // Disable ADC0
+	ADC0->EMUX = 0x5; // Timer triggers interrupt for sequencer0
+	ADC0->SSCTL0 = 0x6; // one sample sequence
+	ADC0->IM = 0x1; // enable interrupts for SS0
+	ADC0->ACTSS = 0x1; // enable SS0 in ADC0
 	
-	//NVIC_EN0_R |= 0x4000; //enable interrupts for ADC0 SS0
+	NVIC_EN0_R = 0x4000; //enable interrupts for ADC0 SS0
 	
 	//GPTM0
-	//TIMER0_CTL_R = 0; //disable
-	//TIMER0_CFG_R = 0; //32-bit
-	//TIMER0_TAMR_R |= 0x2; //periodic mode
-	//TIMER0_TAILR_R = 0x7D00; //Reload value 2ms for 16 MHz clock
-	//TIMER0_CTL_R |= 1; //enable
+	TIMER0->CTL = 0; //disable
+	TIMER0->CFG = 0; //32-bit
+	TIMER0->TAMR = 0x2; //periodic mode
+	TIMER0->TAILR = 0x9C40; //Reload value 2ms for 20 MHz clock
+	TIMER0->CTL = 1; //enable
 
 }
 
