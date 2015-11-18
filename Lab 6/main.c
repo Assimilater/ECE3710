287e4d8772432466d08200ff576503c324b6c31c
Landon Chaz Wilcox <Landon.Wilcox@outlook.com>
2015-11-18 16:57:56 -0700

    continued timer config

--- Lab 6/main.c
+++ Lab 6/main.c
@@ -2,9 +2,11 @@
 #include "../Shared/GPIO.h"
 #include "../Shared/bool.h"
 
-int voltage = 0;
-int i = 0;
+unsigned int voltage = 0;
+unsigned int i = 0;
 
+const unsigned int MAX_FREQUENCY =;
+const unsigned char MAX_VOLTAGE = 5;
 void ADC0SS0_Handler() {
 	ADC0->ISC = 0x1; // acknowledge/clear interrupt
 	TIMER0->ICR = 0x1; // clears the timer expiration flag
@@ -16,7 +18,10 @@ void ADC0SS0_Handler() {
 
 void TIMER1A_Handler() {
 	// Update systick freq. using data from ADC0SS0 handler
-	
+	SysTick->CTRL = 0;
+	SysTick->LOAD = MAX_FREQUENCY * (voltage / MAX_VOLTAGE);
+	SysTick->VAL = 1;
+	SysTick->CTRL = 0x7;
 	// Update DAC Timer
 	
 	voltage = 0;
