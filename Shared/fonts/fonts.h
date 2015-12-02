e906bc828d6e6ef2c53d2e0a93e5782d4b4eae84
John Call <johnkc1992@gmail.com>
2015-12-02 13:18:27 -0700

    fix compiler complaints

--- Shared/fonts/fonts.h
+++ Shared/fonts/fonts.h
@@ -1,26 +1,26 @@
 #ifndef __FONTS_H__
 #define __FONTS_H__
 
-typedef struct {
-	
-} Font;
+//typedef struct {
+//	
+//} Font;
 
-typedef struct {
-	Font* Ubuntu;
-	Font* Terminus;
-	Font* Small;
-	Font* Big;
-	Font* _8x8;
-	Font* Basic_8x8;
-} FontList;
+//typedef struct {
+//	Font* Ubuntu;
+//	Font* Terminus;
+//	Font* Small;
+//	Font* Big;
+//	Font* _8x8;
+//	Font* Basic_8x8;
+//} FontList;
 
-const static FontList Fonts = {
-	//(Font*) Ubuntu,
-	//(Font*) Terminus,
-	//(Font*) Small,
-	//(Font*) Big,
-	//(Font*) _8x8,
-	//(Font*) Basic_8x8
-};
+//const static FontList Fonts = {
+//	//(Font*) Ubuntu,
+//	//(Font*) Terminus,
+//	//(Font*) Small,
+//	//(Font*) Big,
+//	//(Font*) _8x8,
+//	//(Font*) Basic_8x8
+//};
 
 #endif
