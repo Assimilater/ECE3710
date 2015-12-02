1c82ad233c59dd54df99db1851400b2c9a4981e7
John Call <johnkc1992@gmail.com>
2015-12-02 15:24:33 -0700

    write helper functions for accessing fonts

--- Shared/fonts.h
+++ Shared/fonts.h
@@ -2,26 +2,20 @@
 #define __FONTS_H__
 
 typedef struct {
-	unsigned char x, y;
-	char* val;
+	unsigned char** root;
+	unsigned char width, height;
 } font;
 
-//typedef struct {
-//	Font* Ubuntu;
-//	Font* Terminus;
-//	Font* Small;
-//	Font* Big;
-//	Font* _8x8;
-//	Font* Basic_8x8;
-//} FontList;
+typedef struct {
+	font Ubuntu;
+	font Terminus;
+	font Small;
+	font Big;
+	font _8x8;
+	font Basic_8x8;
+} FontList;
 
-//const static FontList Fonts = {
-//	//(Font*) Ubuntu,
-//	//(Font*) Terminus,
-//	//(Font*) Small,
-//	//(Font*) Big,
-//	//(Font*) _8x8,
-//	//(Font*) Basic_8x8
-//};
+const FontList* fonts(void);
+const unsigned char* font_get(const font*, char);
 
 #endif
--- Shared/fonts/fonts.c
+++ Shared/fonts/fonts.c
@@ -6,3 +6,58 @@
 #include "font8x8.h"
 #include "BigFont.h"
 
+//---------------------------------------------------------------------------------------+
+// My implementation of the singleton for accessing fonts                                |
+//---------------------------------------------------------------------------------------+
+const FontList* fonts() {
+	static FontList list;
+	static char init = 0;
+	if (!init) {
+		// Ubuntu
+		list.Ubuntu.root = (unsigned char**) Ubuntu;
+		list.Ubuntu.width = 3; // (24 x 32) / 8 bit char
+		list.Ubuntu.height = 32;
+		
+		// Terminus
+		list.Terminus.root = (unsigned char**) TerminusFont;
+		list.Terminus.width = 1; // (8 x 12) / 8 bit char
+		list.Terminus.height = 12;
+		
+		// Small
+		list.Small.root = (unsigned char**) SmallFont;
+		list.Small.width = 1; // (8 x 12) / 8 bit char
+		list.Small.height = 12;
+		
+		// Big
+		list.Big.root = (unsigned char**) BigFont;
+		list.Big.width = 2; // (16 x 16) / 8 bit char
+		list.Big.height = 16;
+		
+		// 8x8
+		list.Basic_8x8.root = (unsigned char**) font8x8;
+		list.Basic_8x8.width = 1; // (8 x 8) / 8 bit char
+		list.Basic_8x8.height = 1;
+		
+		// 8x8_basic
+		list._8x8.root = (unsigned char**) font8x8_basic;
+		list._8x8.width = 1; // (8 x 8) / 8 bit char
+		list._8x8.height = 1;
+		
+		init = 1;
+	}
+	
+	return &list;
+}
+
+//---------------------------------------------------------------------------------------+
+// Small helper function: takes an ascii char and returns a pointer to font data for it  |
+//---------------------------------------------------------------------------------------+
+const unsigned char* font_get(const font* font, char val) {
+	if (val < ' ' || val > '~') {
+		val = 0; // use space for unknown characters (ie extended ascii)
+	} else {
+		val -= ' ';
+	}
+	
+	return font->root[val];
+}
