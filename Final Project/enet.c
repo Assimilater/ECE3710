a98764ce51df45f4e934487f9885e72d78c8f921
John Call <johnkc1992@gmail.com>
2015-12-03 09:34:29 -0700

    Moving onto our final project now that the LCD/Touchscreen is all in good order

--- Final Project/enet.c
+++ Final Project/enet.c
@@ -2,8 +2,7 @@
 #include "../Shared/GPIO.h"
 #include "enet.h"
 
-void NET_WriteCmd (short address, char control)
-{
+void NET_WriteCmd(short address, char control) {
 	//Phase descriptions start at PG.18 in data sheet
 	//Address Phase
 	SSI0->DR = address >> 8 & 0xFF;
@@ -12,21 +11,18 @@ void NET_WriteCmd (short address, char control)
 	SSI0->DR = control;
 }
 
-void NET_READDATA()
-{
+void NET_READDATA() {
 	NET_WriteCmd(0x0017,0x01); //Socket Interrput Register, read 1 byte
 	
 	//Data Phase
 	SSI0->DR;
 }
 
-void NET_PARSEDATA()
-{
+void NET_PARSEDATA() {
 	
 }
 
-void NET_WRITEDATA()
-{
+void NET_WRITEDATA() {
 	NET_WriteCmd(0x0000,0x00);
 	
 	//Data Phase
