48c30c762e453f43fa9b61cf00a471b9dd13521a
Landon Chaz Wilcox <Landon.Wilcox@outlook.com>
2015-12-02 19:53:49 -0700

    Added stuff to NET functions

--- Final Project/enet.c
+++ Final Project/enet.c
@@ -6,6 +6,15 @@
 void NET_READDATA()
 {
 	
+	//Address Phase
+	SSI0->DR = 0x00;
+	SSI0->DR = 0x00; //both bytes are specifying an address. Don't know what to do here yet
+	
+	//Control Phase  //Don't know what to do for BSB yet
+	SSI0->DR = 0x00; //0bBSB[4:0]000 //last three bits says I'm reading and in Variable length Data mode
+	
+	//Data Phase
+	//Read
 }
 
 void NET_PARSEDATA()
@@ -15,5 +24,13 @@ void NET_PARSEDATA()
 
 void NET_WRITEDATA()
 {
+	//Address Phase
+	SSI0->DR = 0x00;
+	SSI0->DR = 0x00; //both bytes are specifying an address. Don't know what to do here yet
 	
+	//Control Phase  //Don't know what to do for BSB yet
+	SSI0->DR = 0x04; //0bBSB[4:0]100 //last three bits says I'm writing and in Variable length Data mode
+	
+	//Data Phase
+	//Write
 }
