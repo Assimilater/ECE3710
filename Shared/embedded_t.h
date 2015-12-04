667f19553a2ac19b74e84597985d2b523365468e
John Call <johnkc1992@gmail.com>
2015-12-03 19:42:01 -0700

    This was missing from last commit?

---
+++ Shared/embedded_t.h
@@ -0,0 +1,10 @@
+#ifndef __EMBEDDED_T_H__
+#define __EMBEDDED_T_H__
+
+//---------------------------------------------------------------------------------------+
+// Type defentions that should be used for better, more descriptive code                 |
+//---------------------------------------------------------------------------------------+
+typedef unsigned short bytelength;
+typedef unsigned char byte;
+
+#endif
